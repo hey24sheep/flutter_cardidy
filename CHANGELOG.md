3d45edcc6e857505f685aa8cee05396c004e4393
hey24sheep <hey24sheep@gmail.com>
2023-10-12 11:30:22 +0530

    removed dupes, fixed sdk constraint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 ## [1.0.1] - 12 Oct 2023
-* Updated support link in Readme
-* Version bump
+- `pubspec.yaml` updated `sdk constraint`
+- Removed duplicate imports from `cardidy.dart`
+- Updated support link in Readme
+- Version bump
 
 ## [1.0.0] - 16 Nov 2021.
 
--- lib/src/cardidy.dart
+++ lib/src/cardidy.dart
@@ -2,7 +2,6 @@ export 'extensions/extensions.dart';
 export 'models/acard.dart';
 export 'models/aluhn_card.dart';
 export 'models/card_type.dart';
-export 'models/card_type.dart';
 export 'models/cards.dart';
 export 'models/icard.dart';
 export 'models/padded_range.dart';
